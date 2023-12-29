c69bc9e02f3beafd67d25c8db40bd6f2ce378560
Rohithkumar <rohithkumargande@gmail.com>
2023-12-29 16:12:35 +0530

    initial commit

---
+++ Assignment15/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment15.Models
+{
+    [Table("Team")]
+    public class Team
+    {
+        [Key]
+        public int TId {  get; set; }
+        [Required]
+        [StringLength(100)]
+        public string TName { get; set; }
+        [Required]
+        public string TEmail {  get; set; }
+        public string TSlogan { get; set; }
+    }
+}
